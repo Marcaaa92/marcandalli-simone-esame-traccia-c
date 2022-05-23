5dc714a7f78232f5dae4866c3a3968c87a018fb3
Marcaaa92 <78621850+Marcaaa92@users.noreply.github.com>
2022-05-23 14:21:16 +0200

    feat: definited arraylist of piatti and a builder product list

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.util.ArrayList;
 
 /**
  * Hello world!
@@ -6,8 +7,23 @@ package org.example;
  */
 public class App 
 {
+    public static ArrayList<Piatti> piatti = new ArrayList<>();
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        buildProductList();
+        System.out.println(piatti);
+    }
+    static void buildProductList() {
+        piatti.add(new Piatti("carbonara", "Tipico piatto romano", 1, 8.0));
+        piatti.add(new Piatti("Risotto allo zafferano", "Tipico piatto milanese", 2, 7.50));
+        piatti.add(new Piatti("Pizza margherita", "Orgoglio napoletano", 3, 4.50));
+        piatti.add(new Piatti("Pizza capricciosa", "Orgoglio napoletano", 4, 7.50));
+        piatti.add(new Piatti("Cotoletta milanese", "Piatto simbolo di milano", 5, 9.50));
+        piatti.add(new Piatti("Aragosta", "Piatto costoso", 5, 19.90));
+    }
+
+    public static ArrayList<Piatti> getProducts() {
+        return piatti;
     }
 }
