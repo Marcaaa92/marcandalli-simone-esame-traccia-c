9cebce9ada25577da5706e6472a6db2fef7e8c2f
Marcaaa92 <78621850+Marcaaa92@users.noreply.github.com>
2022-05-23 16:07:52 +0200

    feat: finished version with added all 3 function

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,13 +9,12 @@ import java.util.ArrayList;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static ArrayList<Piatti> piatti = new ArrayList<>();
-
+    public static final ArrayList<Piatti> piatti = new ArrayList<>();
+    public static final String allStr = getAll().toString();
     public static void main( String[] args )
     {
-        buildProductList();
         System.out.println(piatti);
         HttpServer server = null;
         try {
@@ -23,7 +22,6 @@ public class App
         } catch (IOException e) {
             e.printStackTrace();
         }
-        buildProductList();
         //server.createContext("/applications/myapp", new MyHandler());
         assert server != null;
         server.createContext("/", new MyHandler());
@@ -36,7 +34,17 @@ public class App
         piatti.add(new Piatti("Pizza margherita", "Orgoglio napoletano", 3, 4.50));
         piatti.add(new Piatti("Pizza capricciosa", "Orgoglio napoletano", 4, 7.50));
         piatti.add(new Piatti("Cotoletta milanese", "Piatto simbolo di milano", 5, 9.50));
-        piatti.add(new Piatti("Aragosta", "Piatto costoso", 5, 19.90));
+        piatti.add(new Piatti("Aragosta", "Piatto costoso", 6, 19.90));
+    }
+    public static StringBuilder getAll(){
+        buildProductList();
+        StringBuilder outputList = new StringBuilder("<ul>");
+        ArrayList<Piatti> all=App.getProducts();
+        for (int i=0; i<all.size(); i++) {
+            outputList.append("<li>Id: ").append(all.get(i).getId()).append(" Name: ").append(all.get(i).getName()).append(" Description: ").append(all.get(i).getDescription()).append(" Price: ").append(all.get(i).getPrice()).append("</li>");
+        }
+        outputList.append("</ul>");
+        return outputList;
     }
 
     public static ArrayList<Piatti> getProducts() {
--- src/main/java/org/example/MyHandler.java
+++ src/main/java/org/example/MyHandler.java
@@ -6,7 +6,6 @@ import java.net.URI;
 import java.util.ArrayList;
 
 class MyHandler implements HttpHandler {
-    static final ArrayList<Piatti> all=App.getProducts();
     public void handle(HttpExchange t) throws IOException {
         URI uri = t.getRequestURI();
         String query = uri.getQuery();
@@ -16,5 +15,82 @@ class MyHandler implements HttpHandler {
         else{
             printResponse("You have not sent nothing", 400, t);
         }
+    }
+    public void runner(String query, HttpExchange t) throws IOException {
+        String[] param;
+        param = query.split("&");
+        if(param.length==1) {
+            String[] value = param[0].split("=");
+            if (value[0].equals("cmd")) {
+                switch (value[1]) {
+                    case "all" -> printResponse(printHTML(printUL("all")), 200, t);
+                    case "more_expensive" -> printResponse(printHTML(printUL("more_expensive")), 200, t);
+                    case "all_sorted" -> printResponse(printHTML(printUL("all_sorted")), 200, t);
+                    default -> printResponse("You have not inserted a correct filter", 400, t);
+                }
+            }
+            else {
+                printResponse("wrong syntax", 400, t);
+            }
+        }
+        else{
+            printResponse("Too much param", 400, t);
+        }
+    }
+    public void printResponse(String text, int errorCode, HttpExchange t) throws IOException {
+        String response = printHTML(text);
+        t.sendResponseHeaders(errorCode, response.length());
+        OutputStream os = t.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+    public String printUL(String action){
+        StringBuilder outputList = null;
+        switch (action) {
+            case "all" -> {
+                return App.allStr;
+            }
+            case "all_sorted" -> {
+                outputList = new StringBuilder("<ul>");
+                ArrayList<Piatti> sorted;
+                sorted = sort();
+                for (int i=0; i<sorted.size(); i++) {
+                    outputList.append("<li>Id: ").append(sorted.get(i).getId()).append(" Name: ").append(sorted.get(i).getName()).append(" Description: ").append(sorted.get(i).getDescription()).append(" Price: ").append(sorted.get(i).getPrice()).append("</li>");
+                }
+                outputList.append("</ul>");
+            }
+            case "more_expensive" -> {
+                outputList = new StringBuilder("<ul>");
+                ArrayList<Piatti> sorted;
+                sorted = sort();
+                outputList.append("<li>Id: ").append(sorted.get(sorted.size() - 1).getId()).append(" Name: ").append(sorted.get(sorted.size() - 1).getName()).append(" Description: ").append(sorted.get(sorted.size() - 1).getDescription()).append(" Price: ").append(sorted.get(sorted.size() - 1).getPrice()).append("</li>");
+                outputList.append("</ul>");
+            }
+        }
+        return outputList.toString();
+    }
+    public String printHTML(String text){
+        String htmlCode="""
+                        <!DOCTYPE html>
+                        <html lang="en">
+                            <head>
+                                <meta charset="UTF-8">
+                                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                <title>Piatti</title>
+                            </head>
+                            <body>
+                                <p>
+                                    """+text+"""
+                                </p>
+                            </body>
+                        </html>
+                """;
+        return htmlCode;
+    }
+    public ArrayList<Piatti> sort(){
+        ArrayList<Piatti> p = App.getProducts();
+        p.sort((Piatti primo, Piatti secondo) -> {if(primo.getPrice()>secondo.getPrice()){return 1;}if(primo.getPrice()<secondo.getPrice()){return -1;}return 0;});
+        return p;
     }
 }
